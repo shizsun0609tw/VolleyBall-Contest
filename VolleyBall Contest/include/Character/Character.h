61961032f18d812e7f2eeb9b6fc7dafbc33802ca
im99014 <egg99014@gmail.com>
2019-06-22 17:52:50 +0800

    writing

--- VolleyBall Contest/include/Character/Character.h
+++ VolleyBall Contest/include/Character/Character.h
@@ -31,7 +31,7 @@ public:
 private:
 	glm::vec3 pos = glm::vec3(0.f, 0.f, 0.f);
 	glm::vec3 angle = glm::vec3(0.f);
-	glm::vec3 size = glm::vec3(0.21.f);
+	glm::vec3 size = glm::vec3(0.21f);
 	/* physics attrib */
 	Mass m = 65;
 	Force f = glm::vec3(0.f);
--- VolleyBall Contest/src/Character/Member.cpp
+++ VolleyBall Contest/src/Character/Member.cpp
@@ -2,26 +2,41 @@
 
 // when spike count falling point, using liear paratmeter
 // input : pos (ball position), velocity (ball velocity)
-// output : probably where to drop
-glm::vec3 Member::spike(glm::vec3 pos, Velocity velocity) {
+// output : vector(character -> probably where to drop)
+glm::vec3 Member::spike(const glm::vec3 pos, const Velocity velocity) {
 	float t = (0.105 - pos.y) / velocity.y; // paratmeter
-	glm::vec3 fallingPoint = pos + t * velocity;
-	return fallingPoint;
+	glm::vec3 forward = pos + t * velocity;
+	forward = forward - this->getPos();
+	return forward;
 }
 
 // when except spike count falling point
 // before fall, calculate the approximate direction
 // start falling, using free fall equation
 // input : pos (ball position), velocity(ball velocity)
-// output : probably where to drop;
-glm::vec3 Member::serve(glm::vec3 pos, Velocity velocity) {
-	glm::vec3 position = 
+// output : vector(character -> probably where to drop)
+glm::vec3 Member::serve(const glm::vec3 pos, const Velocity velocity) {
+	const glm::vec3 position = this->getPos(); // character's position
+	glm::vec3 B = position - pos;
+	glm::vec3 forward;
 	if (velocity.y > 0) { // ball isn't fall yet
+		if (front(pos, position)) { // ball in front of character
 
+		}
+		else { // ball back of character
+
+		}
 	}
 	else { // ball is falling
-
+		float g = 9.80665;
+		float t = glm::sqrt((2 * pos.y - 0.21) / g);
+		forward = pos + t * velocity;
+		forward = position - forward;
+		forward.y = 0.0;
 	}
-	
-	return glm::vec3(1.0);
+	return forward;
+}
+
+bool front() {
+
 }
